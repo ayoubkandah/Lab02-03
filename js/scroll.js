d7291a4ef25b84a02c3cc3313bc0c987c64b4789
ayoubkandah <a.kandah994@gmail.com>
2021-02-16 21:19:30 +0200

    lab03

--- js/scroll.js
+++ js/scroll.js
@@ -1,7 +1,7 @@
 Scroll();
 function Scroll(){
     var introText=document.getElementsByClassName("photo-template");
-   var  screenPos=window.innerHeight/2.589;
+   var  screenPos=window.innerHeight/2;
 for(var x=0;x<introText.length;x++){
    var introPosition=introText[x].getBoundingClientRect().top;
 if(introPosition<screenPos){
